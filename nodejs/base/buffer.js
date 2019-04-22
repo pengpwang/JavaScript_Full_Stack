f3702b4f67e6f1beb4dd2dc34f7bdafe5a1a6e10
pengpwang <pengpwang@sina.com>
2019-04-22 22:44:31 +0800

    add buffer.js

--- nodejs/base/buffer.js
+++ nodejs/base/buffer.js
@@ -1,4 +1,33 @@
 
-console.log(
-    Buffer.from([1,2,3])
-)
+// 1/ 创建buffer
+console.log(Buffer.alloc(5));
+console.log(Buffer.alloc(5, 1));
+console.log(Buffer.allocUnsafe(5));
+console.log(Buffer.from([1,2,3]));
+console.log(Buffer.from('test')); // 默认是utf8
+console.log(Buffer.from('test','base64'));
+
+
+// 2. Buffer类提供的属性和方法（静态属性和静态方法）
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
