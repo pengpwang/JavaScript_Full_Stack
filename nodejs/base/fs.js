f8d46c81dafd2bbcbfd968b3b34c947b806b7836
pengpwang <pengpwang@sina.com>
2019-04-23 22:38:04 +0800

    add fs.js

---
+++ nodejs/base/fs.js
@@ -0,0 +1,41 @@
+
+// 1. fs.readFile();读文件 异步
+// fs.readFileSync(); 同步
+
+
+
+
+// 2. fs.writeFile(); 写文件
+// fs.writeFileSync(); 
+
+
+// 3. fs.stat(); 获取指定文件的信息
+
+
+
+// 4. fs.rename(); 重命名
+
+
+//5 . fs.unlink(); 删除文件
+
+
+// 6. fs.readdir(); 读取文件夹内的所有文件
+
+
+// 7. fs.mkdir(); 创建一个文件夹
+
+
+// 8. fs.rmdir(); 删除文件夹
+
+
+//9.fs.watch(); 监控文件夹或文件的变化   =====>用来做本地构建
+// fs.watchFile();
+
+//10. 流； fs.createReadStream() ******
+
+
+// 11. fs.createWriteStream() ********
+
+
+
+
