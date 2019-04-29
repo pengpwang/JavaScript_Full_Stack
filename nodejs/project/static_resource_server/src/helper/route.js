8ba6fbaaf5d567f27d24202d13d07889267d9737
pengpwang <pengpwang@sina.com>
2019-04-29 09:03:41 +0800

    update static_resource_server

--- nodejs/project/static_resource_server/src/helper/route.js
+++ nodejs/project/static_resource_server/src/helper/route.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
-const handlebars = require('handlebars');
+const path = require('path');
+const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
+const config = require('../config/defaultConfig');
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 
-const source = fs.readFileSync()
+const tplPath = path.join(__dirname, '../template/dir.tpl');  // 除了require()其他尽量使用绝对路径
+const source = fs.readFileSync(tplPath); // 1.后面必须等待全部读取结果； 2.只会执行一次，nodejs项目启动后缓存。3.返回结果Buffer（未指定字符编码）
+const template = Handlebars.compile(source.toString());
 
 module.exports = async function (req, res, filePath) {
     try {
@@ -17,8 +21,16 @@ module.exports = async function (req, res, filePath) {
         }else if(stats.isDirectory()) {
             const files = await readdir(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(files.join(','));
+            res.setHeader('Content-Type', 'text/html');
+
+            const dir = path.relative(config.root, filePath);
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '', // 相对于网站的根路径，当filePath为当前路径，（根路径）时，返回的是空。 ？？？？
+                files
+            };
+
+            res.end(template(data));
         }
     } catch (ex) {
         console.error(ex);
