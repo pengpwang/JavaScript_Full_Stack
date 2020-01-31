f10f9e74e64a30c241c894c47a7449eec036a6fb
wangpengpeng <wangpengpeng@yit.com>
2020-02-01 00:17:05 +0800

    add React

---
+++ React/demo/src/pages/Demo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd';
+
+class Demo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h6>demo</h6>
+        <Button>按钮</Button>
+      </div>
+    )
+  }
+}
+
+export default Demo;
