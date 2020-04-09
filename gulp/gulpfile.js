704d8e779ab9a431bdc3ce402c298985ce880f34
wangpengpeng <wangpengpeng@yit.com>
2020-04-09 16:56:21 +0800

    add

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,7 +1,30 @@
 
+const { src, dest } = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-function defaultTask(cb) {
-  cb();
+exports.default = function() {
+  return src('./src/*.js')
+    // gulp-uglify 插件并不改变文件名
+    // .pipe(uglify())
+    // 因此使用 gulp-rename 插件修改文件的扩展名
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest('./output/'));
 }
 
-exports.default = defaultTask;
+
+// function defaultTask(cb) {
+//   cb();
+// }
+
+// exports.default = defaultTask;
+
+
+// exports.default = function() {
+//   return src('src/*.js')
+//     // gulp-uglify 插件并不改变文件名
+//     .pipe(uglify())
+//     // 因此使用 gulp-rename 插件修改文件的扩展名
+//     .pipe(rename({ extname: '.min.js' }))
+//     .pipe(dest('output/'));
+// }
---
+++ gulp/src/index.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  console.log('hello');
+}
+
+sayHello();
+
+let a = 1;
+let b = 2;
